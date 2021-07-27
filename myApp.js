1c388fbacd44243e031bc692124ac7006f643167
Michael Orzel <56739010+michael-orzel@users.noreply.github.com>
2021-07-27 17:01:33 -0400

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -21,7 +21,10 @@ const createAndSavePerson = (done) => {
 };
 
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+	Person.create(arrayOfPeople, (err, people) => {
+		if(err) return console.log(err);
+		done(null, people);
+	});
 };
 
 const findPeopleByName = (personName, done) => {
